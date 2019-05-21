a250e10f1a7b220bd20f6486dbb4af1f79d6ed5c
Djalma Sebástian <djalmirinha@gmail.com>
2019-05-21 18:52:43 -0300

    Making Pac's mouth change direction

--- src/assets/js/Pacman.sketch.js
+++ src/assets/js/Pacman.sketch.js
@@ -20,35 +20,39 @@ export default (p)=>{
                 switch (ev.key){
                     case 'w':
                         if (pac.xspeed + pac.yspeed == 0) {
-                            pac.yspeed = -1
+                            pac.yspeed = -2
                             pac.xspeed = 0
                         }
-                        pac.nextYSpeed = -1
+	                    pac.direction = - p.HALF_PI
+                        pac.nextYSpeed = -2
                         pac.nextXSpeed = 0
                         break
                     case 'a':
                         if (pac.xspeed + pac.yspeed == 0) {
                             pac.yspeed = 0
-                            pac.xspeed = -1
+                            pac.xspeed = -2
                         }
+	                    pac.direction = p.PI
                         pac.nextYSpeed = 0
-                        pac.nextXSpeed = -1
+                        pac.nextXSpeed = -2
                         break
                     case 's':
                         if (pac.xspeed + pac.yspeed == 0) {
-                            pac.yspeed = 1
+                            pac.yspeed = 2
                             pac.xspeed = 0
                         }
-                        pac.nextYSpeed = 1
+	                    pac.direction = p.HALF_PI
+                        pac.nextYSpeed = 2
                         pac.nextXSpeed = 0
                         break
                     case 'd':
                         if (pac.xspeed + pac.yspeed == 0) {
                             pac.yspeed = 0
-                            pac.xspeed = 1
+                            pac.xspeed = 2
                         }
+                        pac.direction = 0
                         pac.nextYSpeed = 0
-                        pac.nextXSpeed = 1
+                        pac.nextXSpeed = 2
                         break
                 }
             }
